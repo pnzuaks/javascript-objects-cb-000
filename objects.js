626f187fd2887b5cc213a8fc5c1807c78f04ad3e
Patricia Nykaza <pnykaza@calstatela.edu>
2019-01-17 20:09:34 +0000

    Done.

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {'name' : 'song titles'}
+
+function updatePlaylist(obj, artistName, songTitle) {
+  return Object.assign({}, obj, {[name]: title})
+}
+
+function removeFromPlaylist(obj, name) {
+  return Object.assign()
+
+}
